027376e656ce87952baacd79a5087caac531387e
vanshYadav <vanshajjyadava@gmail.com>
2026-07-01 01:36:41 +0530

    implement navigation layout with Navbar, Body, and Profile components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Body from "./components/Body";
 import Feed from "./components/Feed";
 import Login from "./components/Login";
+import Profile from "./components/Profile";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<Body />}>
               <Route path="/feed" element={<Feed />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
             </Route>
           </Routes>
         </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
@@ -7,7 +8,9 @@ const Navbar = () => {
     <>
       <div className="navbar bg-base-100 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevBook</a>
+          <Link to={user ? "/feed" : "/login"} className="btn btn-ghost text-xl">
+            DevBook
+          </Link>
         </div>
         {user && (
           <div className="flex gap-2">
@@ -30,10 +33,10 @@ const Navbar = () => {
                 tabIndex="-1"
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
